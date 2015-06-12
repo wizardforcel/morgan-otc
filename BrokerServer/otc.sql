452ec36602d488322f0e73ff3a86f63bf5e9915b
wizardforcel <562826179@qq.com>
2015-06-12 12:18:35 +0800

    6.12

--- BrokerServer/otc.sql
+++ BrokerServer/otc.sql
@@ -1,33 +1,28 @@
-create table good
+create table broker
+(
+  name varchar(32) primary key
+);
+
+create table order_t
 (
   id int primary key auto_increment,
-  trader_name varchar(32),
-  user_name varchar(32),
   name varchar(32),
-  comment varchar(256),
+  trader varchar(32),
+  broker varchar(32),
   count int,
   price int,
   status int,
-  vip bool,
-  time bigint
+  time bigint,
+  type boolean
 );
 
 create table history
 (
   id int primary key auto_increment,
-  gid int,
-  trader_name varchar(32),
-  user_name varchar(32),
+  seller int,
+  buyer int,
   count int,
-  price int,
   time bigint,
-  foreign key (gid) references good(id)
-);
-
-create table trader
-(
-  name varchar(32) primary key,
-  host varchar(32),
-  port int,
-  app_name varchar(32)
+  foreign key (seller) references order_t(id),
+  foreign key (buyer) references order_t(id)
 );
